674a27f776f767d3582eddf6957985cab5d91e1a
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-30 17:44:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let InputBtn=document.getElementById("form")
+
+   function removeSelectedColor() { 
+    var select = document.getElementById("colorSelect");
+
+    if (select.selectedIndex !== -1) {
+        // Check if any option is selected
+        select.remove(select.selectedIndex);
+    }
+}
+
+InputBtn.addEventListener('click',()=>{
+    removeSelectedColor()
+})
